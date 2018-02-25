67ec04da7cafae8c520ced29a37b1f2485b22aef
liuwenhao <924666990@qq.com>
2018-02-25 19:14:22 +0800

    1、【新增】新增InstrumentView的Demo。

--- app/src/main/java/com/lwh/jackknife/demo/instrumentview/InstrumentViewActivity.java
+++ app/src/main/java/com/lwh/jackknife/demo/instrumentview/InstrumentViewActivity.java
@@ -18,15 +18,44 @@ package com.lwh.jackknife.demo.instrumentview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 
 import com.lwh.jackknife.demo.R;
+import com.lwh.jackknife.util.NumberUtils;
+import com.lwh.jackknife.widget.InstrumentView;
+
+import java.util.Random;
 
 public class InstrumentViewActivity extends Activity {
 
+    private InstrumentView instrumentView;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrument_view);
+        instrumentView = (InstrumentView) findViewById(R.id.instrumentview);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                instrumentView.setRatio(0.1f);
+                instrumentView.setBody("10万吨");
+            }
+        }, 1000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                instrumentView.setRatio(0.3f);
+                instrumentView.setBody("30万吨");
+            }
+        }, 2000);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                instrumentView.setRatio(0.2f);
+                instrumentView.setBody("20万吨");
+            }
+        }, 3000);
     }
 }
